974e2ff386ab581a3f19745b2df0d6417d6f91ff
saidAbdirahman <saidqaaje11@gmail.com>
2024-08-14 08:02:31 +0200

    first commit
    Signed-off-by: saidAbdirahman <saidqaaje11@gmail.com>

---
+++ app/Models/Author.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Author extends Model
+{
+    use HasFactory;
+    public function book(){
+        return $this->belongsToMany(Book::class);
+    }
+}
---
+++ app/Models/Book.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Book extends Model
+{
+    use HasFactory;
+    public function author(){
+        return $this->belongsToMany(Author::class);
+    }
+}
