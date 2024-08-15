d9cff89cee12a54f00f289ccf546a0ec56b1e7be
saidAbdirahman <saidqaaje11@gmail.com>
2024-08-15 20:10:15 +0200

    retieving many data
    Signed-off-by: saidAbdirahman <saidqaaje11@gmail.com>

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,11 +2,22 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Author;
+use Illuminate\Container\Attributes\Auth;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
     public function index(){
-        
+        $all = Author::with('book')->get();
+        // dd($all->toArray());
+        foreach($all as $item){
+            echo $item->name. '<br>';
+            foreach($item->book as $item2){
+                echo $item2->title. '<br>';
+        }
+        echo '<br>';
+    }
+
     }
 }
